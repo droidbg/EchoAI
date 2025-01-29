98f951146578ccc7831c7574fa4aa1be1dbcb0ae
Binni G <41873024+droidbg@users.noreply.github.com>
2025-01-29 09:24:03 +0530

    add. chat input body

--- client/src/components/ChatInput.tsx
+++ client/src/components/ChatInput.tsx
@@ -1,8 +1,14 @@
 const ChatInput = () => {
   return (
-    <div className="w-full bg-gray-300 rounded-xl px-2 py-4 max-h-40 overflow-auto relative ">
-      <textarea className=" border-0 bg-transparent outline-none w-11/12 text-black" />
-      <img src="/send.png" width={20} className="absolute top-4 right-3" />
+    <div className="w-full bg-gray-600 rounded-xl px-2 py-4 max-h-40 overflow-auto relative flex">
+      <textarea
+        rows={1}
+        className=" border-0 bg-transparent outline-none w-11/12 "
+      />
+      <img
+        src="/send.png"
+        className="mx-2 hover:cursor-pointer ease-in duration-100 hover:scale-125 h-8 self-center"
+      />
     </div>
   );
 };
