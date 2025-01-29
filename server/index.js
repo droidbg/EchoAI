7ee21af3f3fa3bd20112169956a00a804332fc2c
Binni G <41873024+droidbg@users.noreply.github.com>
2025-01-29 14:49:30 +0530

    add. post api call to open ai in server

--- server/index.js
+++ server/index.js
@@ -2,18 +2,54 @@ import express from "express";
 import cors from "cors";
 import env from "dotenv";
 import bodyParser from "body-parser";
-// import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
+import * as process from "process";
+env.config();
 
 const app = express();
-
-env.config();
 
 app.use(cors());
 
 app.use(bodyParser.json());
 
-app.listen("3080", () => console.log("listening on port 3080"));
+//Configure OpenAI
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-app.get("/", (req, res) => {
+// Listening port 3080
+app.listen("3080", () =>
+  console.log("listening on port http://localhost:3080/")
+);
+
+app.get("/", (_, res) => {
+  console.log(process.env.OPENAI_API_KEY);
   res.send("Hello World");
+});
+
+app.post("/", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      store: true,
+      messages: [
+        {
+          role: "user",
+          content: message,
+        },
+      ],
+      temperature: 0.5,
+      max_completion_tokens: 100,
+    });
+    console.log(completion);
+
+    completion.then((result) => {
+      console.log(result.choices[0].message);
+      res.send(result.choices[0].message);
+    });
+  } catch (e) {
+    console.log(e);
+    res.send(e).status(400);
+  }
 });
