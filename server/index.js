c81f83f463c909f8343e1974b576ada2556ff9cd
Binni G <41873024+droidbg@users.noreply.github.com>
2025-01-29 09:51:11 +0530

    start the server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 import express from "express";
+import cors from "cors";
+import env from "dotenv";
+import bodyParser from "body-parser";
+// import { Configuration, OpenAIApi } from "openai";
+
+const app = express();
+
+env.config();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.listen("3080", () => console.log("listening on port 3080"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
